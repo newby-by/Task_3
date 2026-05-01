91f3b7a4a027c8d44a4c3d806de82088cf5ac6f3
newby-by <practikum10@gmail.com>
2026-05-01 21:57:29 +0300

    Add order window page

--- pages/__init__.py
+++ pages/__init__.py
@@ -3,6 +3,8 @@ from .base_page import BasePage
 from .forgot_password_page import ForgotPasswordPage
 from .login_page import LoginPage
 from .main_page import MainPage
-from .nav_panel import NavPanelPage
 from .order_list_page import FeedPage
 from .reset_password_page import ResetPasswordPage
+# Panels
+from .nav_panel import NavPanelPage
+from .orders_panel import OrdersPanelPage
